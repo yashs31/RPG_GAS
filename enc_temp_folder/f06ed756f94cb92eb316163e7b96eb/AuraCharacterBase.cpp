70938d3903811260d3dd34fe0b0a53adf787ce51
yashs31 <yashsavoicar@gmail.com>
2025-05-10 16:45:39 +0530

    <Created Aura and GoblinBP>

---
+++ enc_temp_folder/f06ed756f94cb92eb316163e7b96eb/AuraCharacterBase.cpp
@@ -0,0 +1,18 @@
+// Copyright @FLYSPY
+
+
+#include "Character/AuraCharacterBase.h"
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
