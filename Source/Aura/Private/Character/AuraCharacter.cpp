d3374e8149b694593d314525b15d732d5f0a5fe4
yashs31 <yashsavoicar@gmail.com>
2025-05-13 17:28:07 +0530

    <Setup Character camera and the game mode settings>

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -2,4 +2,16 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+AAuraCharacter::AAuraCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0, 400, 0);
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
 
+	//character shoudl not use controlelr rotation
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright @FLYSPY
+
+
+#include "Game/AuraGameModeBase.h"
+
